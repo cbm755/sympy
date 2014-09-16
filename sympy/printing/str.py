1c4eef14b4028ec61b0095b354b252dd529dbdf5
Colin B. Macdonald <macdonald@maths.ox.ac.uk>
2014-09-16 13:14:58 +0100

    Revert "StrPrinter: allow subclasses to change the symbol used for exponents"
    This reverts commit 3027c9cae2c01464bbd84bfe01eeafb6d4185853.

--- sympy/printing/str.py
+++ sympy/printing/str.py
@@ -25,8 +25,6 @@ class StrPrinter(Printer):
 
     _relationals = dict()
 
-    _exponentsymbol = "**"
-
     def parenthesize(self, item, level):
         if precedence(item) <= level:
             return "(%s)" % self._print(item)
@@ -494,13 +492,12 @@ class StrPrinter(Printer):
             # but just check to be sure.
             if e.startswith('(Rational'):
                 return '%s**%s' % (self.parenthesize(expr.base, PREC), e[1:-1])
-        return '%s%s%s' % (self.parenthesize(expr.base, PREC), self._exponentsymbol, e)
+        return '%s**%s' % (self.parenthesize(expr.base, PREC), e)
 
     def _print_MatPow(self, expr):
         PREC = precedence(expr)
-        return '%s%s%s' % (self.parenthesize(expr.base, PREC),
-                           self._exponentsymbol,
-                           self.parenthesize(expr.exp, PREC))
+        return '%s**%s' % (self.parenthesize(expr.base, PREC),
+                         self.parenthesize(expr.exp, PREC))
 
     def _print_Integer(self, expr):
         return str(expr.p)
