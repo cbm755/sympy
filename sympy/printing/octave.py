b16103409eb31adaa22a7326b2fadb144ccfaa81
Colin B. Macdonald <macdonald@maths.ox.ac.uk>
2014-09-18 14:48:42 +0100

    OctaveCodePrinter: remove FIXME comment
    Not relevant to Octave, filed issue #8059 instead.

--- sympy/printing/octave.py
+++ sympy/printing/octave.py
@@ -248,7 +248,6 @@ class OctaveCodePrinter(CodePrinter):
     _print_Tuple = _print_list
 
 
-    # FIXME: ccode (and fcode?) need better bool support
     def _print_BooleanTrue(self, expr):
         return "true"
 
